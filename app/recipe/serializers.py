5ea28bddec84e6d0d9f0647a423a219178737d07
Hojjat <hoj.borhany@gmail.com>
2020-08-25 15:47:56 +0430

    recipe api database model and api is created

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import  serializers
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 class TagSerializer(serializers.ModelSerializer):
     """"serializer for tag object"""
@@ -15,3 +15,27 @@ class IngredientSerializer(serializers.ModelSerializer):
         model = Ingredient
         fields = ('id', 'name')
         read_only_fields = ('id',)
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serialize a Recipe"""
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset = Ingredient.objects.all()
+    )
+
+    tag = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset= Tag.objects.all()
+    )
+    class Meta:
+        model = Recipe
+        fields = ('id', 'title', 'ingredients', 'tag', 'time_minutes', 'price', 'link')
+        read_only_fields = ('id',)
+
+        
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tag = TagSerializer(many=True, read_only=True)
+
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,12 +1,13 @@
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 from recipe import serializers
 
 
 class BaseRecipeAttrViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
     """Base view set for user owned recipe attributes"""
+    
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
@@ -33,5 +34,30 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
 
      queryset = Ingredient.objects.all()
      serializer_class = serializers.IngredientSerializer
+    
 
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage recipe in the database"""
+
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user"""
+        return self.queryset.filter(user=self.request.user)
+    
+    def get_serializer_class(self):
+        """Return appropriate serializer class"""
+        if self.action=='retrieve':
+            return serializers.RecipeDetailSerializer
+        
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new Recipe"""
+        serializer.save(user = self.request.user)
+        
      
